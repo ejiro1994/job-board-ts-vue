29f79357bd65036e826d2198c38b9cb272ab4754
ejiro1994 <ejiro1994@gmail.com>
2024-02-24 09:59:08 +0000

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { reactive, ref } from 'vue'
+const state = reactive({
+  name: 'Ejiro',
+  age: 29 as string | number
+})
+
+const lastName = ref('Akpoduado')
+
+const age = ref<number | string>(29)
+
+const handleButton = () => {
+  state.name = 'Tega'
+}
+</script>
+
+<template>
+
+</template>
